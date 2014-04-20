18ab3bc070f382d4f35cdb841a6381323fd3b7e8
Masanori Usami <nichiyoubinoshisya@gmail.com>
2014-04-20 20:56:49 +0900

    add Calendar

--- Handler/Calendar.hs
+++ Handler/Calendar.hs
@@ -1,6 +1,28 @@
 module Handler.Calendar where
 
 import Import
+import Data.Time
+import Data.Time.Calendar.MonthDay
+import System.Time
 
 getCalendarR :: String -> Handler Html
-getCalendarR = error "Not yet implemented: getCalendarR"
+getCalendarR text = do
+	cal  <- liftIO $ getClockTime
+	cal' <- liftIO $ toCalendarTime cal
+	defaultLayout $ do
+		setTitle "Calendar"
+		[whamlet|<h1>#{show (getMonth cal')}<h2>#{show (getDays cal')}|]
+
+getCalendar :: String
+getCalendar = "test"
+
+getMonth :: CalendarTime -> Int
+getMonth cal = (fromEnum (ctMonth cal)) + 1
+
+getDays :: CalendarTime -> [Int]
+getDays cal = (replicate ((getWeek cal) - 1) 0) ++ [1..length]
+		where length = monthLength (isLeapYear (fromIntegral (ctYear cal))) (getMonth cal)
+
+getWeek :: CalendarTime -> Int
+getWeek cal = (fromEnum (ctWDay cal)) + 1
+
