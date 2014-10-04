55c93be765fb505ce66bcb7a01ec8a5749baf897
Masanori Usami <nichiyoubinoshisya@gmail.com>
2014-10-04 13:49:54 +0900

    add Candidates Handler

---
+++ Handler/Candidates.hs
@@ -0,0 +1,14 @@
+module Handler.Candidates where
+
+import Import
+
+postCandidatesR :: Handler Html
+postCandidatesR = error "Not yet implemented: postCandidatesR"
+-- postCandidatesR = do
+--	((result, widget), enctype) <- runFormPost candidateAForm
+--	case result of
+--		_ -> defaultLayout
+--			[whamlet|
+-- <p>Invalid input, let's try again.
+-- |]
+
