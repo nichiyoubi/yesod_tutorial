d9f75439a1e03ca9d9951897cb6e42ea731f0fcb
Masanori Usami <nichiyoubinoshisya@gmail.com>
2014-10-21 23:05:07 +0900

    add candidates.hamlet

--- Handler/Candidates.hs
+++ Handler/Candidates.hs
@@ -4,23 +4,21 @@ import Import
 
 postCandidatesR :: Handler Html
 postCandidatesR = do
-	textVal0 <- runInputPost $ iopt textField "day0"
-	textVal1 <- runInputPost $ iopt textField "day1"
-	textVal2 <- runInputPost $ iopt textField "day2"
-	textVal3 <- runInputPost $ iopt textField "day3"
-	textVal4 <- runInputPost $ iopt textField "day4"
-	textVal5 <- runInputPost $ iopt textField "day5"
-	textVal6 <- runInputPost $ iopt textField "day6"
-	textVal7 <- runInputPost $ iopt textField "day7"
-	textVal8 <- runInputPost $ iopt textField "day8"
-	textVal9 <- runInputPost $ iopt textField "day9"
-	textVal10 <- runInputPost $ iopt textField "day10"
-	defaultLayout $ do
-		[whamlet|
-#{show textVal1} #{show textVal2}
-<hr>
-<div id="calendar">
-  <div class="back">
-    <a href=@{SchedulingR}>back
-|]
+	mname <- lookupSession "title"
+	case mname of 
+	        Nothing -> do
+			redirect SchedulingR
+		Just name -> do
+			mday0 <- runInputPost $ iopt textField "day0"
+			mday1 <- runInputPost $ iopt textField "day1"
+			mday2 <- runInputPost $ iopt textField "day2"
+			mday3 <- runInputPost $ iopt textField "day3"
+			mday4 <- runInputPost $ iopt textField "day4"
+			mday5 <- runInputPost $ iopt textField "day5"
+			mday6 <- runInputPost $ iopt textField "day6"
+			mday7 <- runInputPost $ iopt textField "day7"
+			mday8 <- runInputPost $ iopt textField "day8"
+			mday9 <- runInputPost $ iopt textField "day9"
+			mday10 <- runInputPost $ iopt textField "day10"
+			defaultLayout $(widgetFile "candidates")
 
