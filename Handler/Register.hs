08b9e23d4a9856b6d5cc69385fee502656ea754d
Masanori Usami <nichiyoubinoshisya@gmail.com>
2014-10-29 00:27:01 +0900

    add RegisterR Handler

---
+++ Handler/Register.hs
@@ -0,0 +1,9 @@
+module Handler.Register where
+
+import Import
+
+getRegisterR :: Int -> Handler Html
+getRegisterR = error "Not yet implemented: getRegisterR"
+
+postRegisterR :: Int -> Handler Html
+postRegisterR = error "Not yet implemented: postRegisterR"
