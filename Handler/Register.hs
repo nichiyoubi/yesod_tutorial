719a0fac50bb2a95847399e6a08de72a5f839f6a
Masanori Usami <nichiyoubinoshisya@gmail.com>
2014-11-17 00:44:13 +0900

    add regsister.hamlet

--- Handler/Register.hs
+++ Handler/Register.hs
@@ -3,12 +3,9 @@ module Handler.Register where
 import Import
 
 getRegisterR :: CandidateListId -> Handler Html
--- getRegisterR = error "Not yet implemented: getRegisterR"
 getRegisterR listId = do
         days <- runDB $ selectList [CandidateDaysListId ==. listId] []
-	defaultLayout [whamlet|
-<h1>test
-|]
+	defaultLayout $(widgetFile "register")
 
 postRegisterR :: CandidateListId -> Handler Html
 postRegisterR = error "Not yet implemented: postRegisterR"
